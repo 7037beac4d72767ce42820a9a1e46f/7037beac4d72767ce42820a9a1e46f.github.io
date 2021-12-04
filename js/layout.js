c834ee90d12252674d966f5d3d9c2f6e5f4d79bf
Junnel Torate <jttorate@gmail.com>
2021-12-04 18:49:09 +0800

    ***

--- js/layout.js
+++ js/layout.js
@@ -6,23 +6,23 @@ $(function () {
         const urlQueries = url.split('/');
         const urlLastQuery = urlQueries[urlQueries.length - 1];
         const index = urlLastQuery.indexOf("index") >= 0;
-        const skill = urlLastQuery.indexOf("skill") >= 0;
-        const service = urlLastQuery.indexOf("service") >= 0;
+        const skills = urlLastQuery.indexOf("skills") >= 0;
+        const services = urlLastQuery.indexOf("services") >= 0;
         const experience = urlLastQuery.indexOf("experience") >= 0;
-        const reference = urlLastQuery.indexOf("reference") >= 0;
+        const references = urlLastQuery.indexOf("references") >= 0;
         const certification = urlLastQuery.indexOf("certification") >= 0;
         const contactMe = urlLastQuery.indexOf("contact-me") >= 0;
 
         if (index || urlLastQuery === "") {
             $("#layout-header header nav ul").find(".nav-index").addClass("active");
-        } else if (skill) {
-            $("#layout-header header nav ul").find(".nav-skill").addClass("active");
-        } else if (service) {
-            $("#layout-header header nav ul").find(".nav-service").addClass("active");
+        } else if (skills) {
+            $("#layout-header header nav ul").find(".nav-skills").addClass("active");
+        } else if (services) {
+            $("#layout-header header nav ul").find(".nav-services").addClass("active");
         } else if (experience) {
             $("#layout-header header nav ul").find(".nav-experience").addClass("active");
-        } else if (reference) {
-            $("#layout-header header nav ul").find(".nav-reference").addClass("active");
+        } else if (references) {
+            $("#layout-header header nav ul").find(".nav-references").addClass("active");
         } else if (certification) {
             $("#layout-header header nav ul").find(".nav-certification").addClass("active");
         } else if (contactMe) {
