c24ad49693ec646385249bd96038ddc450f3f5fe
7037beac4d72767ce42820a9a1e46f <7037beac4d72767ce42820a9a1e46f@gmail.com>
2022-07-10 11:30:23 +0800

    ***

--- js/guestModal.js
+++ js/guestModal.js
@@ -1,87 +1,98 @@
 /** Elements */
-const $guestForm = document.querySelector('#guest-form');
-const $guestFormInput = $guestForm.querySelector('input');
-const $guestFormTextarea = $guestForm.querySelector('textarea');
-const $guestFormButton = $guestForm.querySelector('button');
-const $guestFormError = $guestForm.querySelector('.error');
-const $guestModal = $('#guest-modal');
+const $guestForm = document.querySelector("#guest-form");
+const $guestFormInput = $guestForm.querySelector("input");
+const $guestFormTextarea = $guestForm.querySelector("textarea");
+const $guestFormButton = $guestForm.querySelector("button");
+const $guestFormError = $guestForm.querySelector(".error");
+const $guestModal = $("#guest-modal");
 /** URL */
 const baseUrl = window.location.origin;
 const hostUrl = window.location.host;
 const hrefUrl = window.location.href;
-const pathArray = window.location.pathname.split('/');
+const pathArray = window.location.pathname.split("/");
 /* Get source app URL params if exist */
 const queryString = window.location.search.substring(1);
 const parsedQs = parseQueryString(queryString);
-const appCode = (parsedQs.app ?? 'GLOBAL').toString().trim().toUpperCase();
+const appCode = (parsedQs.app ?? "GLOBAL").toString().trim().toUpperCase();
 let guestModalOpen = false; // Default
 
 $(document).ready(() => {
-	window.scrollTo(window.scrollX, window.scrollY - 1); /** Trigger scroll without scrolling */
+  window.scrollTo(
+    window.scrollX,
+    window.scrollY - 1
+  ); /** Trigger scroll without scrolling */
 
-	$(window).scroll(() => {
-		const lastTwoPath = pathArray.length !== 2 ? pathArray.slice(Math.max(pathArray.length - 2, 1)) : pathArray;
+  $(window).scroll(() => {
+    const lastTwoPath =
+      pathArray.length !== 2
+        ? pathArray.slice(Math.max(pathArray.length - 2, 1))
+        : pathArray;
 
-		if (lastTwoPath[0]?.trim() === "" && lastTwoPath[1]?.trim() === "") {
-			if ($(this).scrollTop() > 50) {
-				openGuestModal(lastTwoPath[0]?.trim(), lastTwoPath[1]?.trim());
-			}
-		} else {
-			openGuestModal(lastTwoPath[0]?.trim(), lastTwoPath[1]?.trim());
-		}
-	});
+    if (lastTwoPath[0]?.trim() === "" && lastTwoPath[1]?.trim() === "") {
+      if ($(this).scrollTop() > 50) {
+        openGuestModal(lastTwoPath[0]?.trim(), lastTwoPath[1]?.trim());
+      }
+    } else {
+      openGuestModal(lastTwoPath[0]?.trim(), lastTwoPath[1]?.trim());
+    }
+  });
 });
 
 openGuestModal = (path1, path2) => {
-	if (!getCookie(`${domainName}_guestId`) && !windowPathNotRequiredGuestModal(path1, path2)) {
-		$guestModal.modal('show');
-		guestModalOpen = true;
-		/* Trigger geolocation permission */
-		// navigatorGeolocation((error, coords) => { });
-	}
-}
+  if (
+    !getCookie(`${domainName}_guestId`) &&
+    !windowPathNotRequiredGuestModal(path1, path2)
+  ) {
+    $guestModal.modal("show");
+    guestModalOpen = true;
+    /* Trigger geolocation permission */
+    // navigatorGeolocation((error, coords) => { });
+  }
+};
 
 guestModalOpenGetStatus = () => {
-	return guestModalOpen;
-}
+  return guestModalOpen;
+};
 
 windowPathNotRequiredGuestModal = (path1, path2) => {
-	const requiredPath1 = ['login'];
-	const requiredPath2 = [''];
-	return requiredPath1.indexOf(path1?.trim()) !== -1 && requiredPath2.indexOf(path2?.trim()) !== -1;
-}
+  const requiredPath1 = ["login"];
+  const requiredPath2 = [""];
+  return (
+    requiredPath1.indexOf(path1?.trim()) !== -1 &&
+    requiredPath2.indexOf(path2?.trim()) !== -1
+  );
+};
 
 /** Guest Modal Form Submit */
-$guestForm.addEventListener('submit', (e) => {
-	e.preventDefault();
+$guestForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-	/** Check if geolocation was allowed */
-	// navigator.permissions.query({ name: 'geolocation' }).then((data) => {
-	// 	if (data.state === 'denied') {
-	// 		$guestFormError.innerHTML = 'Your location is not enabled. Please enable to use this site.';
-	// 	} else {
-			showLoaderModal();
-			$guestFormError.innerHTML = '';
-			/** Disable Button */
-			$guestFormButton.setAttribute('disabled', 'disabled');
+  /** Check if geolocation was allowed */
+  navigator.permissions.query({ name: "geolocation" }).then((data) => {
+    // if (data.state === "denied") {
+    //   $guestFormError.innerHTML =
+    //     "Your location is not enabled. Please enable to use this site.";
+    // } else {
+    showLoaderModal();
+    $guestFormError.innerHTML = "";
+    /** Disable Button */
+    $guestFormButton.setAttribute("disabled", "disabled");
 
-			const guestName = e.target.elements.guestName.value;
-			const guestMsg = e.target.elements.guestMessage.value;
+    const guestName = e.target.elements.guestName.value;
+    const guestMsg = e.target.elements.guestMessage.value;
 
-			guestLogger(appCode, guestName, guestMsg, (error, data) => {
-				$guestFormButton.removeAttribute('disabled');
-				setTimeout(() => {
-					hideLoaderModal();
+    guestLogger(appCode, guestName, guestMsg, (error, data) => {
+      $guestFormButton.removeAttribute("disabled");
+      hideLoaderModal();
 
-					if (!error) {
-						/** Enable Form */
-						$guestFormInput.value = '';
-						$guestFormTextarea.value = '';
-						$guestModal.modal('hide');
-						guestModalOpen = false;
-					}
-				}, 5000);
-			});
-	// 	}
-	// });
+      if (!error) {
+        /** Enable Form */
+        $guestFormInput.value = "";
+        $guestFormTextarea.value = "";
+        $guestModal.modal("hide");
+        guestModalOpen = false;
+      }
+    });
+    // }
+  });
 });
